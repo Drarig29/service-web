d999c56bb1fc772bc7c0d712aaa741256a40b47a
Corentin Girard <corentingirard.dev@gmail.com>
2022-01-06 18:33:51 +0100

    Fix dates

--- Projet/Client/src/app/services/loan.service.ts
+++ Projet/Client/src/app/services/loan.service.ts
@@ -1,29 +1,22 @@
-import { Observable,} from 'rxjs';
+import { Observable, } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { Injectable } from '@angular/core';
-import {Loan} from '../model/loan';
+import { Loan } from '../model/loan';
 import { BaseHttpService } from './baseHttpService';
 
 @Injectable()
 export class LoanService extends BaseHttpService {
-  loan(copyId, userId): Observable<Loan> {
+  loan(copyId: string, userId: string): Observable<Loan> {
     /* to be changed */
-    const loanObj : Loan = {
+    const loanObj: Loan = {
       "userId": userId,
       "copyId": copyId,
-      "loanDate": new Date().toISOString().split('T')[0].replace(/-/,'/')
+      "loanDate": new Date().toISOString().split('T')[0].replace(/-/g, '/')
     }
 
-    return this.http.post<Loan>(`${this.baseUrl}/loans`,loanObj)
-    //je crois que cette partie sert si on utilise le retour du post ? 
-    .pipe(map(()=> null), catchError((err) => { console.log(err); return null; }));
-
-    /*return of({})
-      .pipe(
-        map(() => null),
-        catchError((err) => { console.log(err); return null; })
-      );*/
+    return this.http.post<Loan>(`${this.baseUrl}/loans`, loanObj)
+      .pipe(map(() => null), catchError((err) => { console.log(err); return null; }));
   }
 
   public getAll(): Observable<Loan[]> {
@@ -31,9 +24,7 @@ export class LoanService extends BaseHttpService {
       .get<Loan[]>(`${this.baseUrl}/loans`);
   }
 
-  // TODO : Faire la route API du delete des emprunts 
-
-  public return(copyId):Observable<Loan> {
-    return this.http.delete<Loan>(`${this.baseUrl}/loans/${copyId}`); 
+  public return(copyId: string): Observable<Loan> {
+    return this.http.delete<Loan>(`${this.baseUrl}/loans/${copyId}`);
   }
 }
